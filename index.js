76d84f59e6391770a546224bc6cf926df8b5c0af
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-12-04 08:07:18 +0100

    SDF

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log("users");
+  res.json({ data: "DF" });
+});
+
+app.listen(9000, (err) => {
+  if (err) console.log(err);
+  console.log("app running on Port ", "9000");
+});
